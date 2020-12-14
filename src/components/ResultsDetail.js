35e43ed39829231c43400c67e7747f7af7381d72
barendan <barendan@gmail.com>
2020-12-13 21:16:19 -0500

    finish styling on results list display

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -5,7 +5,7 @@ import ResultsList from './ResultsList';
 const ResultsDetail = ({ result }) => {
 
   return (
-    <View>
+    <View style={styles.container}>
       <Image style={styles.image} source={{ uri: result.image_url }} />
       <Text style={styles.name}>{result.name}</Text>
       <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
@@ -14,10 +14,14 @@ const ResultsDetail = ({ result }) => {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    marginLeft: 15,
+  },
   image: {
     width: 250,
     height: 120,
     borderRadius: 4,
+    marginBottom: 5,
   },
   name: {
     fontWeight: 'bold',
